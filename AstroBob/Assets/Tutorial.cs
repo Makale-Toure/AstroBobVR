9123e361f325b36ea47cc0be042fdc7c309e617b
Makalé Touré <makale.toure03@gmail.com>
2023-10-21 18:22:40 +0200

    v2

---
+++ AstroBob/Assets/Tutorial.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tutorial : MonoBehaviour
+{
+   public GameObject[] popUps;
+   private int popUpIndex;
+
+   void Update(){
+    //for(int)
+   }
+}
